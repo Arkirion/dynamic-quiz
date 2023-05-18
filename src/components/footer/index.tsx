26d72e504e359338717606b2fe788b02492f7065
vismael <xjfw.3.vip@gmail.com>
2023-05-18 00:55:10 -0300

    fix dark themes and delete unused component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,8 +11,8 @@ export const Footer = () => {
 
   return <div className="flex flex-col h-screen justify-between max-h-20">
     <div className="flex-grow border-t border-gray-200"></div>
-    <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-4 dark:bg-gray-800">
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 gap-3">
+    <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-4">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500">
         <li>
           <Link to={`/`}>
             <Button label={"Inicio"} icon={ <IconHome2 /> } />
@@ -22,7 +22,7 @@ export const Footer = () => {
           <Button label={"Reiniciar Juego"} icon={ <IconReload /> } onClick={resetGame}/>
         </li>
       </ul>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500">
       </ul>
     </footer>
   </div>
--- src/pages/quiz-navigator/components/AnswerInput.tsx
+++ src/pages/quiz-navigator/components/AnswerInput.tsx
@@ -80,7 +80,7 @@ export const AnswerInput = ({ quiz, updateQuiz }: AnswerInputProps) => {
           <button
             type="button"
             onClick={(e) => forwardHandler(e)}
-            className="text-black  absolute right-2.5 bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex justify-center">
+            className="text-black  absolute right-2.5 bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 flex justify-center">
             <span className="mr-2 leading-6 tracking-widest">Validar</span>
             <svg width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><rect width='24' height='24' stroke='none' fill='#000000' opacity='0' />
               <g transform="matrix(0.43 0 0 0.43 12 12)" >
